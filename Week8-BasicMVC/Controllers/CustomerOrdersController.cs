8d6b800db5a159ca7127028fcff422f8147d9f3c
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-27 16:20:37 +0300

    Add project files.

---
+++ Week8-BasicMVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week8_BasicMVC.Models.ViewModels;
+using Week8_BasicMVC.Models;
+
+namespace Week8_BasicMVC.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Emre",
+                LastName = "Can",
+                Email = "emrecanterkan06@gmail.com"
+            };
+
+            var orders = new List<Order>
+        {
+            new Order { Id = 1, ProductName = "Laptop", Price = 1500.00M, Quantity = 1 },
+            new Order { Id = 2, ProductName = "Mouse", Price = 20.00M, Quantity = 2 },
+            new Order { Id = 3, ProductName = "Klavye", Price = 50.00M, Quantity = 1 }
+        };
+
+            // ViewModel'i oluşturma
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            // View'a ViewModel'i iletme
+            return View(viewModel);
+        }
+    }
+}
---
+++ Week8-BasicMVC/Models/ViewModels/CustomerOrderViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Week8_BasicMVC.Models.ViewModels
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
